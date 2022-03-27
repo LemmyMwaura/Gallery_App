4a9908fb9f72f08fa1301b4316ebd636866f83cd
Lemmy Mwaura <lemminton@gmail.com>
2022-03-27 12:43:10 +0300

    connected to postgresql db

--- base/models.py
+++ base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+    # Image = 
+    name = models.CharField(max_length=150)
+    description = models.TextField(null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    # location Foreign Key =
+
+    def __str__(self):
+        return self.name
