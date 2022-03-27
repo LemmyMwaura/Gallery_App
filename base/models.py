4fbaf3c599a5fbbe32de9b25d02f0d70826eb9a5
Lemmy Mwaura <lemminton@gmail.com>
2022-03-27 15:45:22 +0300

    added and registered models

--- base/models.py
+++ base/models.py
@@ -1,14 +1,24 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+class Location(models.Model):
+    name = models.CharField(max_length=200)
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 class Image(models.Model):
-    # Image = 
-    name = models.CharField(max_length=150)
+    image = CloudinaryField('image')
     description = models.TextField(null=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-    # location Foreign Key =
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
