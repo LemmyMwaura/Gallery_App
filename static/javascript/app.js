34aadfd2d172a47b906c0557bea21f153606f266
Lemmy Mwaura <lemminton@gmail.com>
2022-03-28 11:03:56 +0300

    pop up modal added

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -1,8 +1,28 @@
-const AllImages = document.querySelectorAll('.picture')
+const allImages = document.querySelectorAll('.picture')
+const overlay = document.getElementById('overlay')
 
-AllImages.forEach((image) => {
+allImages.forEach((image) => {
     image.addEventListener('click', () => {
-        console.log(image.src)
+        let modal = image.parentElement.nextElementSibling
+        openModal(modal)
     })
-});
+})
 
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach((modal) => {
+        closeModal(modal)
+    })
+})
+
+function openModal(modal){
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal){
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
